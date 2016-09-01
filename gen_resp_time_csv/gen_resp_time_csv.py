76aaa44073086eb5e24bc9274c682e9a227a3dbb
taylanksen <tsen@cs.rochester.edu>
2016-09-01 17:07:06 -0400

    Added place holder file for gen_resp_time_csv.py and its examples

---
+++ gen_resp_time_csv/gen_resp_time_csv.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" 
+------------------------------------------------------------------------
+  TODO
+
+------------------------------------------------------------------------
+"""
+
+import csv
+import glob
+import os
+import logging
+
+
+import sys
+import os
+import glob
+import subprocess
+
+
+print('starting gen_resp_time_csv.py')
+#f = open('attendees1.csv')
+#csv_f = csv.reader(f)
+
+#for row in csv_f:
+# print(row)
+
+
